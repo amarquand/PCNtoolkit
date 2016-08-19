51a1846b6b44006d7a0c5f4bf9982fa4c3614539
amarquand <amarquand@gmail.com>
2016-08-19 09:39:59 +0200

    more basic restructuring

---
+++ tests/testblr.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import sys
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
+
+sys.path.append('/home/mrstats/andmar/sfw/pyspat')
+from bayesreg import blr
+
+X = np.arange(0,10,0.1)
+N = len(X)
+dimpoly = 3
+
+# this also works but is less like matlab (transposed)
+#Phi = np.zeros((dimpoly,X.shape[0]))
+#colid = np.arange(0,1)
+#for d in range(1,dimpoly+1):
+#    print colid
+#    Phi[colid,:] = X ** d
+#    colid += 1
+
+# generate some data
+Phi = np.zeros((X.shape[0],dimpoly))
+colid = np.arange(0,1)
+for d in range(1,dimpoly+1):
+    Phi[:,colid] = np.vstack(X ** d)
+    colid += 1
+
+b = [0.5, -0.1, 0.005] # true regression coefficients
+s2 = 0.05 # noise variance
+
+y = Phi.dot(b) + np.random.normal(0,s2,N)
+
+yid = range(0,N,1)
+
+hyp0 = np.zeros(2)
+hyp0 = np.zeros(4)
+B = blr()
+B.post(hyp0,Phi[yid,:],y[yid])
+B.loglik(hyp0,Phi[yid,:],y[yid])
+B.dloglik(hyp0,Phi[yid,:],y[yid])
+hyp = B.estimate(hyp0,Phi[yid,:],y[yid])
+
+yhat,s2 = B.predict(hyp0,Phi[yid,:],y[yid],Phi)
+
+plt.plot(X,y)
+plt.plot(X,yhat)
+plt.show()
+#sp.optimize.check_grad(B.loglik, B.dloglik, np.array([0, 0]), Phi,y)
+
+#out = sp.optimize.fmin_cg(B.loglik,hyp0,B.dloglik,(Phi,y),full_output=1)
+#hyp = np.exp(out[0])
+vari = 1/hyp
+
+print B.nlZ
+print vari
+print B.m
