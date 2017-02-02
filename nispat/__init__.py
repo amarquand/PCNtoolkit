6bff48da2daae6b81d1c0796af4599a9730405a7
amarquand <amarquand@gmail.com>
2017-02-02 16:09:47 +0100

    package and script functionality
    - split model estimation away from main()
    - reimplemented dual checks for imports

--- nispat/__init__.py
+++ nispat/__init__.py
@@ -1,6 +1,6 @@
 import bayesreg
 import trendsurf
 import gp
-from .bayesreg import BLR
-from .gp import GPR, covSqExp
-from .fileio import load_nifti, save_nifti, create_mask
+import normative
+import utils
+import fileio
