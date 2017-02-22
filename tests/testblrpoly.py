bug fix: BLR gradients
Fixed a bug in the implementation of the BLR class (the gradients were
not computed properly for the alpha  precision parameters

tests/testblrpoly.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import sys
+sys.path.append('/home/mrstats/andmar/sfw/nispat/nispat')
 import numpy as np
 import scipy as sp
 from matplotlib import pyplot as plt
 
 #sys.path.append('/home/mrstats/andmar/sfw/nispat/nispat')
 from bayesreg import BLR
-from gp import GPR, covSqExp
+from gp import GPR, CovSqExp
 
 X = np.arange(0,10,0.1)
 N = len(X)
@@ -49,23 +50,23 @@ plt.plot(X,y)
 plt.plot(X,yhat)
 plt.show()
 
-# test GPR
-y = y-y.mean()
-hyp0 = np.zeros(3)
-G = GPR(hyp0, covSqExp, Phi[yid,:], y[yid])
-G.loglik(hyp0, covSqExp, Phi[yid,:], y[yid])
-G.dloglik(hyp0, covSqExp, Phi[yid,:], y[yid])
-hyp = G.estimate(hyp0,covSqExp, Phi[yid,:], y[yid])
-
-yhat,s2 = G.predict(hyp0,Phi[yid,:],y[yid],Phi)
-
-plt.plot(X,y)
-plt.plot(X,yhat)
-plt.show()
-#sp.optimize.check_grad(B.loglik, B.dloglik, np.array([0, 0]), Phi,y)
-
-#out = sp.optimize.fmin_cg(B.loglik,hyp0,B.dloglik,(Phi,y),full_output=1)
-#hyp = np.exp(out[0])
+## test GPR
+#y = y-y.mean()
+#hyp0 = np.zeros(3)
+#G = GPR(hyp0, covSqExp, Phi[yid,:], y[yid])
+#G.loglik(hyp0, covSqExp, Phi[yid,:], y[yid])
+#G.dloglik(hyp0, covSqExp, Phi[yid,:], y[yid])
+#hyp = G.estimate(hyp0,covSqExp, Phi[yid,:], y[yid])
+#
+#yhat,s2 = G.predict(hyp0,Phi[yid,:],y[yid],Phi)
+#
+#plt.plot(X,y)
+#plt.plot(X,yhat)
+#plt.show()
+##sp.optimize.check_grad(B.loglik, B.dloglik, np.array([0, 0]), Phi,y)
+#
+##out = sp.optimize.fmin_cg(B.loglik,hyp0,B.dloglik,(Phi,y),full_output=1)
+##hyp = np.exp(out[0])
 vari = 1/hyp
 
 print B.nlZ