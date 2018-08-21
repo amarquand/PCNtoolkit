f56605896fbed91e15b3c9a3b066a25a7be61efe
Thomas Wolfers <th.wolfers@gmail.com>
2018-08-21 11:00:31 +0200

    update nispat
    normative_parallel has changed and improved a bit. 
    
    The normative analyse and normative visualize tools provide some basics for analysis and visualization but are not fully functional. I am not sure if we should include those to the master branch

--- nispat/gp.py
+++ nispat/gp.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 from __future__ import division
 
-import os
-import sys
 import numpy as np
 from scipy import optimize
 from numpy.linalg import solve, LinAlgError
@@ -10,16 +8,7 @@ from numpy.linalg import cholesky as chol
 from six import with_metaclass
 from abc import ABCMeta, abstractmethod
 
-
-try:  # Run as a package if installed    
-    from nispat.utils import squared_dist
-except ImportError:
-    pass
-    path = os.path.abspath(os.path.dirname(__file__))
-    if path not in sys.path:
-        sys.path.append(path)
-    del path
-    from utils import squared_dist
+from nispat.utils import squared_dist
 
 # --------------------
 # Covariance functions
@@ -266,7 +255,7 @@ class GPR:
         self.hyp = np.nan
         self.nlZ = np.nan
         self.tol = tol          # not used at present
-        self.n_iter = n_iter
+        self.n_iter = n_iter    # not used at present
         self.verbose = verbose
 
         if (hyp is not None) and (X is not None) and (y is not None):
@@ -288,6 +277,10 @@ class GPR:
         if len(X.shape) == 1:
             X = X[:, np.newaxis]
         self.N, self.D = X.shape
+
+        if not self._updatepost(hyp, covfunc):
+            print("hyperparameters have not changed, using exising posterior")
+            return
 
         # hyperparameters
         sn2 = np.exp(2*hyp[0])       # noise variance
@@ -397,9 +390,9 @@ class GPR:
         """ Function to make predictions from the model
         """
 
-        # reestimate posterior (avoids numerical problems with optimizer)
-        self.post(hyp, self.covfunc, X, y)
-        
+        if self._updatepost(hyp, self.covfunc):
+            self.post(hyp, self.covfunc, X, y)
+
         # hyperparameters
         sn2 = np.exp(2*hyp[0])     # noise variance
         theta = hyp[1:]            # (generic) covariance hyperparameters
