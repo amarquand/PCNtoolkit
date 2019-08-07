e3f5ab7a008131653d88079c00148befe5f99045
Seyed Mostafa Kia <m.kia83@gmail.com>
2019-08-07 14:21:29 +0200

    More flexibility to choose the mthod

--- tests/testHBR.py
+++ tests/testHBR.py
@@ -17,13 +17,10 @@ import matplotlib.pyplot as plt
 training_samples_num = 200
 test_samples_num = 81
 configparam = dict()
-configparam['model_type'] = 'lin'
+
 configparam['confounds'] = dict()
 configparam['confounds']['train'] = np.random.randint(0,2,[training_samples_num,2]) 
 configparam['confounds']['test'] = np.random.randint(0,2,[test_samples_num,2])
-with open('configs.pkl', 'wb') as file:
-    pickle.dump(configparam,file)
-
 
 X = np.random.randint(10,91,training_samples_num)
 Y = np.zeros([training_samples_num,])
@@ -36,26 +33,94 @@ Ys = np.zeros([test_samples_num,])
 Ys[configparam['confounds']['test'][:,0]==0,] = Xs[configparam['confounds']['test'][:,0]==0,] * 0.2 + np.random.randn(np.sum(configparam['confounds']['test'][:,0]==0))
 Ys[configparam['confounds']['test'][:,0]==1,] = Xs[configparam['confounds']['test'][:,0]==1,] * 0.5 + 2 + 5 * np.random.randn(np.sum(configparam['confounds']['test'][:,0]==1))
 
-
-
 # Running the model
+configparam['model_type'] = 'lin_rand_int'
+with open('configs.pkl', 'wb') as file:
+    pickle.dump(configparam,file)
 nm = norm_init(X, Y, alg='hbr', configparam='configs.pkl')
 nm.estimate(X, Y)
+yhat_rand_int, s2_rand_int = nm.predict(X, Y, Xs)
 
-yhat, s2 = nm.predict(X, Y, Xs)
+configparam['model_type'] = 'lin_rand_int_slp'
+with open('configs.pkl', 'wb') as file:
+    pickle.dump(configparam,file)
+nm = norm_init(X, Y, alg='hbr', configparam='configs.pkl')
+nm.estimate(X, Y)
+yhat_rand_int_slp, s2_rand_int_slp = nm.predict(X, Y, Xs)
 
+configparam['model_type'] = 'lin_rand_int_slp_nse'
+with open('configs.pkl', 'wb') as file:
+    pickle.dump(configparam,file)
+nm = norm_init(X, Y, alg='hbr', configparam='configs.pkl')
+nm.estimate(X, Y)
+yhat_rand_int_slp_nse, s2_rand_int_slp_nse = nm.predict(X, Y, Xs)
+
+configparam['model_type'] = 'poly2'
+with open('configs.pkl', 'wb') as file:
+    pickle.dump(configparam,file)
+nm = norm_init(X, Y, alg='hbr', configparam='configs.pkl')
+nm.estimate(X, Y)
+yhat_poly2, s2_poly2 = nm.predict(X, Y, Xs)
+
+plt.subplot(2, 2, 1)
 plt.scatter(Xs[configparam['confounds']['test'][:,0]==0,],Ys[configparam['confounds']['test'][:,0]==0,])
 plt.scatter(Xs[configparam['confounds']['test'][:,0]==1,],Ys[configparam['confounds']['test'][:,0]==1,])
-plt.plot(Xs[configparam['confounds']['test'][:,0]==0,],yhat[[configparam['confounds']['test'][:,0]==0,]])
+plt.plot(Xs[configparam['confounds']['test'][:,0]==0,],yhat_rand_int[[configparam['confounds']['test'][:,0]==0,]])
 plt.fill_between(Xs[configparam['confounds']['test'][:,0]==0,], 
-                 yhat[configparam['confounds']['test'][:,0]==0,] - np.sqrt(s2[configparam['confounds']['test'][:,0]==0,]), 
-                 yhat[configparam['confounds']['test'][:,0]==0,] + np.sqrt(s2[configparam['confounds']['test'][:,0]==0,]),
+                 yhat_rand_int[configparam['confounds']['test'][:,0]==0,] - np.sqrt(s2_rand_int[configparam['confounds']['test'][:,0]==0,]), 
+                 yhat_rand_int[configparam['confounds']['test'][:,0]==0,] + np.sqrt(s2_rand_int[configparam['confounds']['test'][:,0]==0,]),
                  color='gray', alpha=0.2)
-plt.plot(Xs[configparam['confounds']['test'][:,0]==1,],yhat[[configparam['confounds']['test'][:,0]==1,]])
+plt.plot(Xs[configparam['confounds']['test'][:,0]==1,],yhat_rand_int[[configparam['confounds']['test'][:,0]==1,]])
 plt.fill_between(Xs[configparam['confounds']['test'][:,0]==1,], 
-                 yhat[configparam['confounds']['test'][:,0]==1,] - np.sqrt(s2[configparam['confounds']['test'][:,0]==1,]), 
-                 yhat[configparam['confounds']['test'][:,0]==1,] + np.sqrt(s2[configparam['confounds']['test'][:,0]==1,]),
+                 yhat_rand_int[configparam['confounds']['test'][:,0]==1,] - np.sqrt(s2_rand_int[configparam['confounds']['test'][:,0]==1,]), 
+                 yhat_rand_int[configparam['confounds']['test'][:,0]==1,] + np.sqrt(s2_rand_int[configparam['confounds']['test'][:,0]==1,]),
                  color='gray', alpha=0.2)
+plt.title('Lin: Random Intercept')
+
+plt.subplot(2, 2, 2)
+plt.scatter(Xs[configparam['confounds']['test'][:,0]==0,],Ys[configparam['confounds']['test'][:,0]==0,])
+plt.scatter(Xs[configparam['confounds']['test'][:,0]==1,],Ys[configparam['confounds']['test'][:,0]==1,])
+plt.plot(Xs[configparam['confounds']['test'][:,0]==0,],yhat_rand_int_slp[[configparam['confounds']['test'][:,0]==0,]])
+plt.fill_between(Xs[configparam['confounds']['test'][:,0]==0,], 
+                 yhat_rand_int_slp[configparam['confounds']['test'][:,0]==0,] - np.sqrt(s2_rand_int_slp[configparam['confounds']['test'][:,0]==0,]), 
+                 yhat_rand_int_slp[configparam['confounds']['test'][:,0]==0,] + np.sqrt(s2_rand_int_slp[configparam['confounds']['test'][:,0]==0,]),
+                 color='gray', alpha=0.2)
+plt.plot(Xs[configparam['confounds']['test'][:,0]==1,],yhat_rand_int_slp[[configparam['confounds']['test'][:,0]==1,]])
+plt.fill_between(Xs[configparam['confounds']['test'][:,0]==1,], 
+                 yhat_rand_int_slp[configparam['confounds']['test'][:,0]==1,] - np.sqrt(s2_rand_int_slp[configparam['confounds']['test'][:,0]==1,]), 
+                 yhat_rand_int_slp[configparam['confounds']['test'][:,0]==1,] + np.sqrt(s2_rand_int_slp[configparam['confounds']['test'][:,0]==1,]),
+                 color='gray', alpha=0.2)
+plt.title('Lin: Random Intercept and Slope')
+
+plt.subplot(2, 2, 3)
+plt.scatter(Xs[configparam['confounds']['test'][:,0]==0,],Ys[configparam['confounds']['test'][:,0]==0,])
+plt.scatter(Xs[configparam['confounds']['test'][:,0]==1,],Ys[configparam['confounds']['test'][:,0]==1,])
+plt.plot(Xs[configparam['confounds']['test'][:,0]==0,],yhat_rand_int_slp_nse[[configparam['confounds']['test'][:,0]==0,]])
+plt.fill_between(Xs[configparam['confounds']['test'][:,0]==0,], 
+                 yhat_rand_int_slp_nse[configparam['confounds']['test'][:,0]==0,] - np.sqrt(s2_rand_int_slp_nse[configparam['confounds']['test'][:,0]==0,]), 
+                 yhat_rand_int_slp_nse[configparam['confounds']['test'][:,0]==0,] + np.sqrt(s2_rand_int_slp_nse[configparam['confounds']['test'][:,0]==0,]),
+                 color='gray', alpha=0.2)
+plt.plot(Xs[configparam['confounds']['test'][:,0]==1,],yhat_rand_int_slp_nse[[configparam['confounds']['test'][:,0]==1,]])
+plt.fill_between(Xs[configparam['confounds']['test'][:,0]==1,], 
+                 yhat_rand_int_slp_nse[configparam['confounds']['test'][:,0]==1,] - np.sqrt(s2_rand_int_slp_nse[configparam['confounds']['test'][:,0]==1,]), 
+                 yhat_rand_int_slp_nse[configparam['confounds']['test'][:,0]==1,] + np.sqrt(s2_rand_int_slp_nse[configparam['confounds']['test'][:,0]==1,]),
+                 color='gray', alpha=0.2)
+plt.title('Lin: Random Intercept and Slope and Noise')
+
+plt.subplot(2, 2, 4)
+plt.scatter(Xs[configparam['confounds']['test'][:,0]==0,],Ys[configparam['confounds']['test'][:,0]==0,])
+plt.scatter(Xs[configparam['confounds']['test'][:,0]==1,],Ys[configparam['confounds']['test'][:,0]==1,])
+plt.plot(Xs[configparam['confounds']['test'][:,0]==0,],yhat_poly2[[configparam['confounds']['test'][:,0]==0,]])
+plt.fill_between(Xs[configparam['confounds']['test'][:,0]==0,], 
+                 yhat_poly2[configparam['confounds']['test'][:,0]==0,] - np.sqrt(s2_poly2[configparam['confounds']['test'][:,0]==0,]), 
+                 yhat_poly2[configparam['confounds']['test'][:,0]==0,] + np.sqrt(s2_poly2[configparam['confounds']['test'][:,0]==0,]),
+                 color='gray', alpha=0.2)
+plt.plot(Xs[configparam['confounds']['test'][:,0]==1,],yhat_poly2[[configparam['confounds']['test'][:,0]==1,]])
+plt.fill_between(Xs[configparam['confounds']['test'][:,0]==1,], 
+                 yhat_poly2[configparam['confounds']['test'][:,0]==1,] - np.sqrt(s2_poly2[configparam['confounds']['test'][:,0]==1,]), 
+                 yhat_poly2[configparam['confounds']['test'][:,0]==1,] + np.sqrt(s2_poly2[configparam['confounds']['test'][:,0]==1,]),
+                 color='gray', alpha=0.2)
+plt.title('Poly2: Random Intercept and Slope and noise')
 
 ########################### TESTING NON-LINEAR HBR ############################
 
