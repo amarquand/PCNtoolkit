e7b08bcc4236675b1190e0cd34e05c44fda9f28c
Seyed Mostafa Kia <m.kia83@gmail.com>
2019-08-07 14:21:29 +0200

    More flexibility to choose the mthod

--- nispat/hbr.py
+++ nispat/hbr.py
@@ -50,23 +50,41 @@ class HBR:
             # Random intercepts
             intercepts = pm.Normal('intercepts', mu=mu_prior_intercept, sigma=sigma_prior_intercept, shape=self.site_num)
             
-            # Random slopes
-            slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(2,self.site_num))
-            
             # Expected value
-            if model_type == 'lin':
+            if model_type == 'lin_rand_int':
+                # Random slopes
+                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(2,))
+                y_hat = intercepts[self.s] + self.a * slopes[self.g]
+                # Model error
+                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100)
+                sigma_y = sigma_error
+            elif model_type == 'lin_rand_int_slp':
+                # Random slopes
+                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(2,self.site_num))
                 y_hat = intercepts[self.s] + self.a * slopes[self.g, self.s]
+                # Model error
+                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100)
+                sigma_y = sigma_error
+            elif model_type == 'lin_rand_int_slp_nse':
+                # Random slopes
+                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(2,self.site_num))
+                y_hat = intercepts[self.s] + self.a * slopes[self.g, self.s]
+                # Model error
+                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(2,self.site_num))
+                sigma_y = sigma_error[self.g, self.s]      
             elif model_type == 'poly2':
+                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(2,self.site_num))
                 mu_prior_slope_2 = pm.Normal('mu_prior_slope_2', mu=0., sigma=1e5)
                 sigma_prior_slope_2 = pm.HalfCauchy('sigma_prior_slope_2', 5)
                 slopes_2 = pm.Normal('slopes_2', mu=mu_prior_slope_2, sigma=sigma_prior_slope_2, shape=(2,self.site_num))
                 y_hat = intercepts[self.s] + self.a * slopes[self.g, self.s] + self.a**2 * slopes_2[self.g, self.s]
+                # Model error
+                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(2,self.site_num))
+                sigma_y = sigma_error[self.g, self.s]
             else:
                 raise ValueError('Unknown method:' + model_type)
             
-            # Model error
-            sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(2,self.site_num))
-            sigma_y = sigma_error[self.g, self.s]
+            
             
             # Data likelihood
             y_like = pm.Normal('y_like', mu=y_hat, sigma=sigma_y, observed=y)
