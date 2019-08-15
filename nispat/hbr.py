adaf50a4d4262dda23166481f54ba9f11bd386b0
Seyed Mostafa Kia <m.kia83@gmail.com>
2019-08-15 11:18:07 +0200

    Poly2 and noise terms are adjusted

--- nispat/hbr.py
+++ nispat/hbr.py
@@ -39,19 +39,11 @@ class HBR:
         self.site_num = len(np.unique(site_id))
         self.gender_num = len(np.unique(gender))
         self.model_type = model_type
-        if model_type == 'nn':
-            age = np.expand_dims(age ,axis = 1)
-            n_hidden = 2
-            n_data = 1
-            init_1 = pm.floatX(np.random.randn(n_data, n_hidden))
-            init_out = pm.floatX(np.random.randn(n_hidden))
-            std_init_1 = pm.floatX(np.ones([n_data, n_hidden]))
-            std_init_out = pm.floatX(np.ones([n_hidden,]))
-        self.a = theano.shared(age)
         self.s = theano.shared(site_id)
         self.g = theano.shared(gender)
-        with pm.Model() as model:
-            if model_type != 'nn':
+        self.a = theano.shared(age)
+        if model_type != 'nn':
+            with pm.Model() as model:
                 # Priors
                 mu_prior_intercept = pm.Normal('mu_prior_intercept', mu=0., sigma=1e5)
                 sigma_prior_intercept = pm.HalfCauchy('sigma_prior_intercept', 5)
@@ -61,45 +53,56 @@ class HBR:
                 # Random intercepts
                 intercepts = pm.Normal('intercepts', mu=mu_prior_intercept, sigma=sigma_prior_intercept, shape=(self.gender_num,self.site_num))
             
-            # Expected value
-            if model_type == 'lin_rand_int':
-                # Random slopes
-                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,))
-                y_hat = intercepts[self.g, self.s] + self.a * slopes[self.g]
-                # Model error
-                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100)
-                sigma_y = sigma_error
-            elif model_type == 'lin_rand_int_slp':
-                # Random slopes
-                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,self.site_num))
-                y_hat = intercepts[self.g, self.s] + self.a * slopes[self.g, self.s]
-                # Model error
-                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100)
-                sigma_y = sigma_error
-            elif model_type == 'lin_rand_int_slp_nse':
-                # Random slopes
-                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,self.site_num))
-                y_hat = intercepts[self.g, self.s] + self.a * slopes[self.g, self.s]
-                # Model error
-                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(self.gender_num,self.site_num))
-                sigma_y = sigma_error[self.g, self.s]      
-            elif model_type == 'lin_rand_int_nse':
-                # Random slopes
-                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,))
-                y_hat = intercepts[self.g, self.s] + self.a * slopes[self.g]
-                # Model error
-                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(self.gender_num,self.site_num))
-                sigma_y = sigma_error[self.g, self.s]
-            elif model_type == 'poly2':
-                slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,self.site_num))
-                mu_prior_slope_2 = pm.Normal('mu_prior_slope_2', mu=0., sigma=1e5)
-                sigma_prior_slope_2 = pm.HalfCauchy('sigma_prior_slope_2', 5)
-                slopes_2 = pm.Normal('slopes_2', mu=mu_prior_slope_2, sigma=sigma_prior_slope_2, shape=(self.gender_num,self.site_num))
-                y_hat = intercepts[self.g, self.s] + self.a * slopes[self.g, self.s] + self.a**2 * slopes_2[self.g, self.s]
-                # Model error
-                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(self.gender_num,self.site_num))
-                sigma_y = sigma_error[self.g, self.s]
-            elif model_type == 'nn':
+                # Expected value
+                if model_type == 'lin_rand_int':
+                    # Random slopes
+                    slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,))
+                    y_hat = intercepts[(self.g, self.s)] + self.a * slopes[(self.g)]
+                    # Model error
+                    sigma_error = pm.Uniform('sigma_error', lower=0, upper=100)
+                    sigma_y = sigma_error
+                elif model_type == 'lin_rand_int_slp':
+                    # Random slopes
+                    slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,self.site_num))
+                    y_hat = intercepts[(self.g, self.s)] + self.a * slopes[(self.g, self.s)]
+                    # Model error
+                    sigma_error = pm.Uniform('sigma_error', lower=0, upper=100)
+                    sigma_y = sigma_error
+                elif model_type == 'lin_rand_int_slp_nse':
+                    # Random slopes
+                    slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,self.site_num))
+                    y_hat = intercepts[(self.g, self.s)] + self.a * slopes[(self.g, self.s)]
+                    # Model error
+                    sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(self.site_num,))
+                    sigma_y = sigma_error[(self.s)]      
+                elif model_type == 'lin_rand_int_nse':
+                    # Random slopes
+                    slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,))
+                    y_hat = intercepts[(self.g, self.s)] + self.a * slopes[(self.g)]
+                    # Model error
+                    sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(self.site_num,))
+                    sigma_y = sigma_error[(self.s)]
+                elif model_type == 'poly2':
+                    slopes = pm.Normal('slopes', mu=mu_prior_slope, sigma=sigma_prior_slope, shape=(self.gender_num,))
+                    mu_prior_slope_2 = pm.Normal('mu_prior_slope_2', mu=0., sigma=1e5)
+                    sigma_prior_slope_2 = pm.HalfCauchy('sigma_prior_slope_2', 5)
+                    slopes_2 = pm.Normal('slopes_2', mu=mu_prior_slope_2, sigma=sigma_prior_slope_2, shape=(self.gender_num,))
+                    y_hat = intercepts[(self.g, self.s)] + self.a * slopes[(self.g)] + self.a**2 * slopes_2[(self.g)]
+                    # Model error
+                    sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(self.site_num,))
+                    sigma_y = sigma_error[(self.s)]
+                # Data likelihood
+                y_like = pm.Normal('y_like', mu=y_hat, sigma=sigma_y, observed=y)
+        elif model_type == 'nn':
+            age = np.expand_dims(age ,axis = 1)
+            self.a = theano.shared(age)
+            n_hidden = 2
+            n_data = 1
+            init_1 = pm.floatX(np.random.randn(n_data, n_hidden))
+            init_out = pm.floatX(np.random.randn(n_hidden))
+            std_init_1 = pm.floatX(np.ones([n_data, n_hidden]))
+            std_init_out = pm.floatX(np.ones([n_hidden,]))
+            with pm.Model() as model:
                 weights_in_1_grp = pm.Normal('w_in_1_grp', 0, sd=1., 
                                  shape=(n_data, n_hidden), 
                                  testval=init_1)
@@ -114,8 +117,7 @@ class HBR:
                 weights_1_out_grp_sd = pm.HalfNormal('w_1_out_grp_sd', sd=1., 
                                           shape=(n_hidden,), 
                                           testval=std_init_out)
-                # Separate weights for each different model, just add a 3rd dimension
-                # of weights
+                # Separate weights for each different model
                 weights_in_1_raw = pm.Normal('w_in_1', 
                                              shape=(self.gender_num, self.site_num, n_data, n_hidden))
                 # Non-centered specification of hierarchical model
@@ -125,17 +127,13 @@ class HBR:
                                               shape=(self.gender_num, self.site_num, n_hidden))
                 weights_1_out = weights_1_out_raw[self.g, self.s,:] * weights_1_out_grp_sd + weights_1_out_grp
                 # Build neural-network using tanh activation function
-                act_1 = pm.math.tanh(theano.tensor.batched_dot(age, weights_in_1))
+                act_1 = pm.math.tanh(theano.tensor.batched_dot(self.a, weights_in_1))
                 y_hat = theano.tensor.batched_dot(act_1, weights_1_out)
                 
-                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(self.gender_num,self.site_num))
-                sigma_y = sigma_error[(self.g, self.s)]
-                
-            else:
-                raise ValueError('Unknown method:' + model_type) 
-            
-            # Data likelihood
-            y_like = pm.Normal('y_like', mu=y_hat, sigma=sigma_y, observed=y)
+                sigma_error = pm.Uniform('sigma_error', lower=0, upper=100, shape=(self.site_num,))
+                sigma_y = sigma_error[(self.s)]
+                # Data likelihood
+                y_like = pm.Normal('y_like', mu=y_hat, sigma=sigma_y, observed=y)
             
         self.model = model
 
@@ -153,6 +151,9 @@ class HBR:
         if pred == 'single':
             if self.model_type == 'nn':
                 age = np.expand_dims(age ,axis = 1)
+                self.a = theano.shared(age)
+                self.s = theano.shared(site_id)
+                self.g = theano.shared(gender)
             with self.model:
                 self.a.set_value(age)
                 self.s.set_value(site_id)
@@ -169,8 +170,10 @@ class HBR:
                 elif self.model_type == 'poly2':
                     temp[i,:] = age[i]**2 * self.trace['mu_prior_slope_2'] + age[i] * self.trace['mu_prior_slope'] + self.trace['mu_prior_intercept']
                 elif self.model_type == 'nn':
-                    act_1 = pm.math.tanh(age[i,:] * self.trace['w_in_1_grp'])
-                    temp[i,:] = theano.tensor.batched_dot(act_1, self.trace['w_1_out_grp'])
+                    act_1 = np.tanh(age[i] * self.trace['w_in_1_grp'])
+                    for j in range(self.trace.nchains * samples):
+                        temp[i,j] = np.dot(np.squeeze(act_1[j,:,:]), self.trace['w_1_out_grp'][j,:])
+                    print(i)
                     
             pred_mean = temp.mean(axis=1)
             pred_var = temp.var(axis=1)
