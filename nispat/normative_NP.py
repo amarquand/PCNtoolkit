ed1f2e1aa82367fcbac42bd3d2419ef769c2cdce
Seyed Mostafa Kia <m.kia83@gmail.com>
2019-09-05 16:53:28 +0200

    Minor adjustment in inputs and outputs

--- nispat/normative_NP.py
+++ nispat/normative_NP.py
@@ -34,6 +34,7 @@ from architecture import Encoder, Decoder
 from nispat.utils import compute_pearsonr, explained_var, compute_MSLL
 from nispat.utils import extreme_value_prob, extreme_value_prob_fit, ravel_2D, unravel_2D
 from nispat import fileio
+import os
 
  
 
@@ -43,14 +44,16 @@ def get_args(*args):
     ############################ Parsing inputs ###############################
     
     parser = argparse.ArgumentParser(description='Neural Processes (NP) for Deep Normative Modeling')
-    parser.add_argument("-r", help="Training response pickle file address", 
+    parser.add_argument("-r", help="Training response nifti file address", 
                         required=True, dest="respfile", default=None)
     parser.add_argument("-c", help="Training covariates pickle file address", 
                         required=True, dest="covfile", default=None)
     parser.add_argument("--tc", help="Test covariates pickle file address", 
                         required=True, dest="testcovfile", default=None)
-    parser.add_argument("--tr", help="Test response pickle file address", 
-                        required=True, dest="testrespfile", default=None)
+    parser.add_argument("--tr", help="Test response nifti file address", 
+                        dest="testrespfile", default=None)
+    parser.add_argument("--mask", help="Mask nifti file address", 
+                        dest="mask", default=None)
     parser.add_argument("-o", help="Output directory address", dest="outdir", default=None)
     parser.add_argument('-m', type=int, default=10, dest='m',
                         help='number of fixed-effect estimations')
@@ -60,12 +63,17 @@ def get_args(*args):
                         help='number of epochs to train')
     parser.add_argument('--device', type=str, default='cuda', dest='device',
                         help='Either cpu or cuda')
-    parser.add_argument('--fxestimator', type=str, default='MT', dest='estimator',
+    parser.add_argument('--fxestimator', type=str, default='ST', dest='estimator',
                         help='Fixed-effect estimator type.')
 
     args = parser.parse_args()
 
-
+    if (args.respfile == None or args.covfile == None or args.testcovfile == None):
+        raise(ValueError, "Training response nifti file, Training covariates pickle file, and \
+              Test covariates pickle file must be specified.")
+    if (args.outdir == None):
+        args.outdir = os.getcwd()
+        
     cuda = args.device=='cuda' and torch.cuda.is_available()
     args.device = torch.device("cuda" if cuda else "cpu")
     args.kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
@@ -73,20 +81,23 @@ def get_args(*args):
     return args
     
 def estimate(args):
+    torch.set_default_dtype(torch.float32)
+    
     print('Loading the input Data ...')
     responses = fileio.load_nifti(args.respfile, vol=True).transpose([3,0,1,2])
+    response_shape = responses.shape
     with open(args.covfile, 'rb') as handle:
-        covariates = pickle.load(handle)['covariates']
-    test_responses = fileio.load_nifti(args.testrespfile, vol=True).transpose([3,0,1,2])
+        covariates = pickle.load(handle)['covariates'] 
     with open(args.testcovfile, 'rb') as handle:
         test_covariates = pickle.load(handle)['test_covariates']
-    
-    torch.set_default_dtype(torch.float32)
-       
-    mask = fileio.create_mask(responses[0,:,:,:], mask=None)
-    
-    response_shape = responses.shape
-    test_responses_shape = test_responses.shape
+    if args.mask is not None:
+        mask = fileio.load_nifti(args.mask, vol=True)
+        mask = fileio.create_mask(mask, mask=None)
+    else: 
+        mask = fileio.create_mask(responses[0,:,:,:], mask=None)
+    if args.testrespfile is not None:
+        test_responses = fileio.load_nifti(args.testrespfile, vol=True).transpose([3,0,1,2])
+        test_responses_shape = test_responses.shape
     
     print('Normalizing the input Data ...')
     covariates_scaler = StandardScaler()
@@ -94,7 +105,9 @@ def estimate(args):
     test_covariates = covariates_scaler.transform(test_covariates)
     response_scaler = MinMaxScaler()
     responses = unravel_2D(response_scaler.fit_transform(ravel_2D(responses)), response_shape)
-    test_responses = unravel_2D(response_scaler.transform(ravel_2D(test_responses)), test_responses_shape)
+    if args.testrespfile is not None:
+        test_responses = unravel_2D(response_scaler.transform(ravel_2D(test_responses)), test_responses_shape)
+        test_responses = np.expand_dims(test_responses, axis=1)
     
     factor = args.m
     
@@ -103,8 +116,8 @@ def estimate(args):
                          responses.shape[2], responses.shape[3]], dtype=np.float32)
     x_all = np.zeros([covariates.shape[0], factor, covariates.shape[1]], dtype=np.float32)
     x_context_test = np.zeros([test_covariates.shape[0], factor, test_covariates.shape[1]], dtype=np.float32)
-    y_context_test = np.zeros([test_responses.shape[0], factor, test_responses.shape[1], 
-                         test_responses.shape[2], test_responses.shape[3]], dtype=np.float32)
+    y_context_test = np.zeros([test_covariates.shape[0], factor, responses.shape[1], 
+                         responses.shape[2], responses.shape[3]], dtype=np.float32)
     
     print('Estimating the fixed-effects ...')
     for i in range(factor):
@@ -130,7 +143,6 @@ def estimate(args):
     
     x_all = x_context
     responses = np.expand_dims(responses, axis=1).repeat(factor, axis=1)
-    test_responses = np.expand_dims(test_responses, axis=1)
     
     ################################## TRAINING #################################  
     
@@ -160,7 +172,7 @@ def estimate(args):
                 loss.backward()
                 train_loss += loss.item()
                 optimizer.step()
-            print('Epoch: %d, Loss:%f' %(k, train_loss))
+            print('Epoch: %d, Loss:%f, Average Loss:%f' %(k, train_loss, train_loss/y_hat.shape[0]))
             k += 1
             
     ################################## Evaluation #################################
@@ -170,42 +182,46 @@ def estimate(args):
     with torch.no_grad():
         y_hat, z_all, z_context, y_sigma = model(torch.tensor(x_context_test, device = args.device),
                                                  torch.tensor(y_context_test, device = args.device), n = 15)
-        test_loss = np_loss(y_hat, torch.tensor(test_responses, device = args.device), z_all, z_context).item()
-        print('Test loss:%f' %(test_loss))
+        
+    if args.testrespfile is not None:   
+        test_loss = np_loss(y_hat[0:test_responses_shape[0],:], 
+                            torch.tensor(test_responses, device = args.device), 
+                            z_all, z_context).item()
+        print('Average Test Loss:%f' %(test_loss/test_responses_shape[0]))
          
-    RMSE = np.sqrt(np.mean((test_responses - y_hat.cpu().numpy())**2, axis = 0)).squeeze() * mask
-    SMSE = RMSE ** 2 / np.var(test_responses, axis=0).squeeze()
-    Rho, pRho = compute_pearsonr(test_responses.squeeze(), y_hat.cpu().numpy().squeeze())
-    EXPV = explained_var(test_responses.squeeze(), y_hat.cpu().numpy().squeeze()) * mask
-    MSLL = compute_MSLL(test_responses.squeeze(), y_hat.cpu().numpy().squeeze(), 
-                        y_sigma.cpu().numpy().squeeze()**2, train_mean = test_responses.mean(0), 
-                        train_var = test_responses.var(0)).squeeze() * mask
-                        
-    NPMs = (test_responses - y_hat.cpu().numpy()) / (y_sigma.cpu().numpy())
-    NPMs = NPMs.squeeze()
-    NPMs = NPMs * mask
-    NPMs = np.nan_to_num(NPMs)
-    
-    temp=NPMs.reshape([NPMs.shape[0],NPMs.shape[1]*NPMs.shape[2]*NPMs.shape[3]])
-    EVD_params = extreme_value_prob_fit(temp, 0.01)
-    abnormal_probs = extreme_value_prob(EVD_params, temp, 0.01)
+        RMSE = np.sqrt(np.mean((test_responses - y_hat[0:test_responses_shape[0],:].cpu().numpy())**2, axis = 0)).squeeze() * mask
+        SMSE = RMSE ** 2 / np.var(test_responses, axis=0).squeeze()
+        Rho, pRho = compute_pearsonr(test_responses.squeeze(), y_hat[0:test_responses_shape[0],:].cpu().numpy().squeeze())
+        EXPV = explained_var(test_responses.squeeze(), y_hat[0:test_responses_shape[0],:].cpu().numpy().squeeze()) * mask
+        MSLL = compute_MSLL(test_responses.squeeze(), y_hat[0:test_responses_shape[0],:].cpu().numpy().squeeze(), 
+                            y_sigma[0:test_responses_shape[0],:].cpu().numpy().squeeze()**2, train_mean = test_responses.mean(0), 
+                            train_var = test_responses.var(0)).squeeze() * mask
+                            
+        NPMs = (test_responses - y_hat[0:test_responses_shape[0],:].cpu().numpy()) / (y_sigma[0:test_responses_shape[0],:].cpu().numpy())
+        NPMs = NPMs.squeeze()
+        NPMs = NPMs * mask
+        NPMs = np.nan_to_num(NPMs)
+        
+        temp=NPMs.reshape([NPMs.shape[0],NPMs.shape[1]*NPMs.shape[2]*NPMs.shape[3]])
+        EVD_params = extreme_value_prob_fit(temp, 0.01)
+        abnormal_probs = extreme_value_prob(EVD_params, temp, 0.01)
     
     ############################## SAVING RESULTS #################################
-    
-    if args.outdir is not None:
-        print('Saving Results to: %s' %(args.outdir))
-        exfile = args.testrespfile
-        y_hat = y_hat.squeeze().cpu().numpy()
-        y_hat = response_scaler.inverse_transform(ravel_2D(y_hat))
-        y_hat = y_hat[:,mask.flatten()]
-        fileio.save(y_hat.T, args.outdir + 
-                    '/yhat.nii.gz', example=exfile, mask=mask)
-        ys2 = y_sigma.squeeze().cpu().numpy()
-        ys2 = response_scaler.inverse_transform(ravel_2D(ys2))
-        ys2 = ys2**2
-        ys2 = ys2[:,mask.flatten()]
-        fileio.save(ys2.T, args.outdir + 
-                    '/ys2.nii.gz', example=exfile, mask=mask) 
+
+    print('Saving Results to: %s' %(args.outdir))
+    exfile = args.respfile
+    y_hat = y_hat.squeeze().cpu().numpy()
+    y_hat = response_scaler.inverse_transform(ravel_2D(y_hat))
+    y_hat = y_hat[:,mask.flatten()]
+    fileio.save(y_hat.T, args.outdir + 
+                '/yhat.nii.gz', example=exfile, mask=mask)
+    ys2 = y_sigma.squeeze().cpu().numpy()
+    ys2 = response_scaler.inverse_transform(ravel_2D(ys2))
+    ys2 = ys2**2
+    ys2 = ys2[:,mask.flatten()]
+    fileio.save(ys2.T, args.outdir + 
+                '/ys2.nii.gz', example=exfile, mask=mask)
+    if args.testrespfile is not None:  
         NPMs = ravel_2D(NPMs)[:,mask.flatten()]
         fileio.save(NPMs.T, args.outdir +  
                     '/Z.nii.gz', example=exfile, mask=mask)
@@ -221,11 +237,11 @@ def estimate(args):
                     '/expv.nii.gz', example=exfile, mask=mask)
         fileio.save(MSLL.flatten()[mask.flatten()], args.outdir +  
                     '/msll.nii.gz', example=exfile, mask=mask)
-        
-        with open(args.outdir +'model.pkl', 'wb') as handle:
-             pickle.dump({'model':model, 'reg':reg, 'covariates_scaler':covariates_scaler,
-                          'response_scaler': response_scaler, 'EVD_params':EVD_params,
-                          'abnormal_probs':abnormal_probs}, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    with open(args.outdir +'model.pkl', 'wb') as handle:
+         pickle.dump({'model':model, 'covariates_scaler':covariates_scaler,
+                      'response_scaler': response_scaler, 'EVD_params':EVD_params,
+                      'abnormal_probs':abnormal_probs}, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
 ###############################################################################
     print('DONE!')
