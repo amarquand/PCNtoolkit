559c8580f19cda7c19ff4acb0a84e15bfa10cfa4
Andre Marquand <amarquand@gmail.com>
2019-09-18 15:31:12 +0200

    update install_requires

--- setup.py
+++ setup.py
@@ -9,9 +9,20 @@ setup(name='nispat',
       license='GNU GPLv3',
       packages=find_packages(),
       install_requires=[
+          'numpy',
+          'pandas',
+          'argparse'
+          'nibabel',
+          'tempfile,
+          'scipy',
+          'six',
+          're',
+          'glob',
+          'abc',
+          'subprocess',
           'sklearn', 
           'torch>=1.1.0', 
           'pymc3>=3.7', 
-          'Theano>=1.0.4' 
+          'Theano>=1.0.4'           
       ],
       zip_safe=False)
