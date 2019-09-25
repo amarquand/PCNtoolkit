706719a8127a1809ed25f77de64185bef9e7249f
Seyed Mostafa Kia <m.kia83@gmail.com>
2019-09-25 16:53:07 +0200

    EVD prob computation is modified

--- nispat/utils.py
+++ nispat/utils.py
@@ -305,10 +305,7 @@ def extreme_value_prob(params, NPM, perc):
         temp = temp[t - n_perc:]
         temp = temp[0:int(np.floor(0.90*temp.shape[0]))]
         m[i] = np.mean(temp)
-    if params[0] <= 0:  # if the shape is right tailed for extreme values
         probs = genextreme.cdf(m,*params)
-    elif params[0] > 0: # if the shape is left tailed for extreme values
-        probs = 1 - genextreme.cdf(m,*params)
     return probs
 
 def ravel_2D(a):
