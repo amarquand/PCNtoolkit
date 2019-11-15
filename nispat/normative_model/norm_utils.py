b077831b4dac55fcf8865dc4463b44272525d83c
Seyed Mostafa Kia <m.kia83@gmail.com>
2019-11-15 16:51:59 +0100

    Single-task NP is added to normative.

--- nispat/normative_model/norm_utils.py
+++ nispat/normative_model/norm_utils.py
@@ -2,6 +2,7 @@ from norm_blr import NormBLR
 from norm_gpr import NormGPR
 from norm_rfa import NormRFA
 from norm_hbr import NormHBR
+from norm_np import NormNP
 
 def norm_init(X, y=None, theta=None, alg='gpr', configparam=None):
     if alg == 'gpr':
@@ -12,6 +13,8 @@ def norm_init(X, y=None, theta=None, alg='gpr', configparam=None):
         nm = NormRFA(X, y, theta, configparam)
     elif alg == 'hbr':
         nm = NormHBR(X, y, configparam)
+    elif alg == 'np':
+        nm = NormNP(X, y, configparam)
     else:
         raise(ValueError, "Algorithm " + alg + " not known.")
         
