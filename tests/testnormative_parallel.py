91127a1da2faf8cc3fde9b3bf8403ecd32ac0bbf
Seyed Mostafa Kia <m.kia83@gmail.com>
2020-02-13 17:31:57 +0100

    Slight changes

--- tests/testnormative_parallel.py
+++ tests/testnormative_parallel.py
@@ -6,35 +6,32 @@ Created on Wed Jan 18 14:34:06 2017
 """
 #import nispat
 import os
-import sys
 import time
-sys.path.append('/home/preclineu/andmar/sfw/nispat/nispat')
-from normative_parallel import execute_nm, collect_nm, delete_nm
+from nispat.normative_parallel import execute_nm, collect_nm, delete_nm
 
-#wdir = '/home/mrstats/andmar/py.sandbox/normative_oslo/'
-#respfile = os.path.join(wdir, 'ICA100_oslo15_v2_spaces.txt')
-#covfile = os.path.join(wdir, 'cov_oslo15_spaces.txt')
-wdir = '/home/mrstats/seykia/data/nispat_test/'
-respfile = os.path.join(wdir, 'responses.txt')
-covfile = os.path.join(wdir, 'covariates.txt')
+data_dir = '/home/preclineu/andmar/py.sandbox/normative_oslo/'
+respfile = os.path.join(data_dir, 'ICA100_oslo15_v2_spaces.txt')
+covfile = os.path.join(data_dir, 'cov_oslo15_spaces.txt')
+
 cvfolds = 2
 #estimate(respfile, covfile, cvfolds=cvfolds)
 
-python_path = '/home/mrstats/seykia/.conda/envs/python3/bin/python'
-normative_path = '/home/mrstats/seykia/Code/nispat/nispat/normative.py'
+python_path = '/.../.conda/envs/python3/bin/python'
+normative_path = '/.../nispat/nispat/normative.py'
+processing_dir= '/.../'
 job_name = 'nmp_test'
 batch_size = 10
 memory = '4gb'
 duration = '01:00:00'
 cluster = 'torque'
 
-execute_nm(wdir, python_path, normative_path, job_name, covfile,  respfile,
+execute_nm(processing_dir, python_path, normative_path, job_name, covfile,  respfile,
            batch_size, memory, duration, cluster_spec=cluster, 
-           cv_folds=cvfolds, log_path=wdir)#, alg='rfa')#, configparam=4)
+           cv_folds=cvfolds, log_path=processing_dir)#, alg='rfa')#, configparam=4)
 
 print("waiting for jobs to finish ...")
 time.sleep(60)
 
-#collect_nm(wdir, collect=True)
-#delete_nm(wdir)
+collect_nm(processing_dir, collect=True)
+#delete_nm(procedssing_dir)
 
