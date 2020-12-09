649247da15fd501020a835e6799726942f318923
Seyed Mostafa Kia <m.kia83@gmail.com>
2020-12-09 22:40:25 +0100

    None is replaced with hcauchy for std f noise.

--- pcntoolkit/hbr.py
+++ pcntoolkit/hbr.py
@@ -269,7 +269,7 @@ def hbr(X, y, batch_effects, batch_effects_size, configs, trace=None):
                 if trace is not None: # Used for transferring the priors
                     sigma_noise = from_posterior('sigma_noise', 
                                                     trace['sigma_noise'], 
-                                                    distribution=None)
+                                                    distribution='hcauchy')
                     #upper_bound =  np.percentile(trace['sigma_noise'], 95)
                     #sigma_noise = pm.Uniform('sigma_noise', lower=0, upper=upper_bound, shape=(batch_effects_size))
                                                     
@@ -288,7 +288,7 @@ def hbr(X, y, batch_effects, batch_effects_size, configs, trace=None):
             if trace is not None: # Used for transferring the priors
                 sigma_noise = from_posterior('sigma_noise', 
                                                     trace['sigma_noise'], 
-                                                    distribution=None)
+                                                    distribution='hcauchy')
                 #upper_bound =  np.percentile(trace['sigma_noise'], 95)
                 #sigma_noise = pm.Uniform('sigma_noise', lower=0, upper=upper_bound)
             else:
@@ -547,7 +547,7 @@ def nn_hbr(X, y, batch_effects, batch_effects_size, configs, trace=None):
             else: # homoscedastic noise:
                 if trace is not None: # Used for transferring the priors
                     sigma_noise = from_posterior('sigma_noise', trace['sigma_noise'], 
-                                                 distribution=None)
+                                                 distribution='hcauchy')
                 else:
                     sigma_noise = pm.Uniform('sigma_noise', lower=0, upper=100, shape=(batch_effects_size))
                     
@@ -564,7 +564,7 @@ def nn_hbr(X, y, batch_effects, batch_effects_size, configs, trace=None):
             if trace is not None: # Used for transferring the priors
                 sigma_noise = from_posterior('sigma_noise', 
                                              trace['sigma_noise'], 
-                                             distribution=None)
+                                             distribution='hcauchy')
             else:
                 sigma_noise = pm.Uniform('sigma_noise', lower=0, upper=100)
             sigma_y = theano.tensor.zeros(y.shape)
