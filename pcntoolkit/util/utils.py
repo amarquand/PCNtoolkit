87f09e1320612d43008db90433561b206336cfc5
Seyed Mostafa Kia <m.kia83@gmail.com>
2021-12-31 14:40:37 +0100

    Interactive parallel execution is added.

--- pcntoolkit/util/utils.py
+++ pcntoolkit/util/utils.py
@@ -1390,3 +1390,23 @@ def cartesian_product(arrays):
         arr[...,i] = a
         
     return arr.reshape(-1, la)
+
+
+def yes_or_no(question):
+    
+    """
+    Utility function for getting yes/no action from the user.
+    
+    :param question: String for user query.
+    
+    :return: Boolean of True for 'yes' and False for 'no'.
+    
+
+    """
+    
+    while "the answer is invalid":
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        if reply[:1] == 'y':
+            return True
+        if reply[:1] == 'n':
+            return False
