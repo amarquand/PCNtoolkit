b5a9745eec0ccb070a15c56d648186e1009f7e5e
Seyed Mostafa Kia <m.kia83@gmail.com>
2022-02-20 19:04:18 +0100

    Some paths in norm_utils are corrected.

--- pcntoolkit/normative_model/norm_utils.py
+++ pcntoolkit/normative_model/norm_utils.py
@@ -1,8 +1,15 @@
-from norm_blr import NormBLR
-from norm_gpr import NormGPR
-from norm_rfa import NormRFA
-from norm_hbr import NormHBR
-from norm_np import NormNP
+try:  # run as a package if installed
+    from pcntoolkit.normative_model.norm_blr import NormBLR
+    from pcntoolkit.normative_model.norm_gpr import NormGPR
+    from pcntoolkit.normative_model.norm_rfa import NormRFA
+    from pcntoolkit.normative_model.norm_hbr import NormHBR
+    from pcntoolkit.normative_model.norm_np import NormNP
+except:
+    from norm_blr import NormBLR
+    from norm_gpr import NormGPR
+    from norm_rfa import NormRFA
+    from norm_hbr import NormHBR
+    from norm_np import NormNP
 
 def norm_init(X, y=None, theta=None, alg='gpr', **kwargs):
     if alg == 'gpr':
