2b3ded575deae145e3bbd1aa227b46e148a20fa1
Seyed Mostafa Kia <m.kia83@gmail.com>
2023-02-17 14:39:43 +0100

    A few corrections for HBR defaults

--- pcntoolkit/normative_model/norm_hbr.py
+++ pcntoolkit/normative_model/norm_hbr.py
@@ -95,21 +95,25 @@ class NormHBR(NormBase):
             if self.configs['linear_sigma']:
                 if 'random_noise' in kwargs.keys():
                     print("The keyword \'random_noise\' is deprecated. It is now automatically replaced with \'random_intercept_sigma\', because sigma is linear")
-                    self.configs['random_intercept_sigma'] = kwargs.pop('random_noise','False') == 'True'
+                    self.configs['random_intercept_sigma'] = kwargs.pop('random_noise','True') == 'True'
             elif 'random_noise' in kwargs.keys():
                 print("The keyword \'random_noise\' is deprecated. It is now automatically replaced with \'random_sigma\', because sigma is fixed")
-                self.configs['random_sigma'] = kwargs.pop('random_noise','False') == 'True'
+                self.configs['random_sigma'] = kwargs.pop('random_noise','True') == 'True'
             if 'random_slope' in kwargs.keys():
                 print("The keyword \'random_slope\' is deprecated. It is now automatically replaced with \'random_intercept_mu\'")
-                self.configs['random_slope_mu'] = kwargs.pop('random_slope','False') == 'True'
+                self.configs['random_slope_mu'] = kwargs.pop('random_slope','True') == 'True'
             ##### End Deprecations 
 
 
         ## Default parameters
         self.configs['linear_mu'] = kwargs.pop('linear_mu','True') == 'True'
+        self.configs['random_mu'] = kwargs.pop('random_mu','True') == 'True'
         self.configs['random_intercept_mu'] = kwargs.pop('random_intercept_mu','True') == 'True'
         self.configs['random_slope_mu'] = kwargs.pop('random_slope_mu','True') == 'True'
         self.configs['random_sigma'] = kwargs.pop('random_sigma','True') == 'True'
+        self.configs['centered_sigma'] = kwargs.pop('centered_sigma','True') == 'True'
+        self.configs['centered_slope_mu'] = kwargs.pop('centered_slope_mu','True') == 'True'
+        self.configs['centered_intercept_mu'] = kwargs.pop('centered_intercept_mu','True') == 'True'
         ## End default parameters
 
         self.hbr = HBR(self.configs)
