891f4b06d46c26ea6969a317a109fc3981f0ac4d
Seyed Mostafa Kia <m.kia83@gmail.com>
2023-02-19 14:21:02 +0100

    norm_hbr documentation is added.

--- pcntoolkit/__init__.py
+++ pcntoolkit/__init__.py
@@ -2,3 +2,5 @@ from . import trendsurf
 from . import normative
 from . import normative_parallel
 from . import normative_NP
+
+__version__ = "0.20"
--- pcntoolkit/normative_model/norm_hbr1.py
+++ pcntoolkit/normative_model/norm_hbr1.py
@@ -34,10 +34,8 @@ except ImportError:
 
 
 class NormHBR(NormBase):
-        
-    """ HBR multi-batch normative modelling class. By default, this function 
-    estimates a linear model with random intercept, random slope, and random 
-    homoscedastic noise.
+    
+    """ HBR multi-batch normative modelling class
     
     :param X: [N×P] array of clinical covariates
     :param y: [N×1] array of neuroimaging measures
@@ -53,39 +51,6 @@ class NormHBR(NormBase):
         and 'bspline' (defauls is 'linear').
     :param likelihood: specifies the type of likelihood among 'Normal' 'SHASHb','SHASHo',
         and 'SHASHo2' (defauls is normal).
-    :param linear_mu: Boolean (default='True') to decide whether the mean (mu) is
-        parametrized on a linear function (thus changes with covariates) or it is fixed.
-    :param linear_sigma: Boolean (default='False') to decide whether the variance (sigma) is
-        parametrized on a linear function (heteroscedastic noise) or it is fixed for
-        each batch (homoscedastic noise).
-    :param linear_epsilon: Boolean (default='False') to decide the parametrization
-        of epsilon for the SHASH likelihood that controls its skewness. 
-        If True, epsilon is  parametrized on a linear function 
-        (thus changes with covariates) otherwise it is fixed for each batch.
-    :param linear_delta: Boolean (default='False') to decide the parametrization
-        of delta for the SHASH likelihood that controls its kurtosis. 
-        If True, delta is  parametrized on a linear function 
-        (thus changes with covariates) otherwise it is fixed for each batch.
-    :param random_intercept_{parameter}: if parameters mu (default='True'), 
-        sigma (default='False'), epsilon (default='False'), and delta (default='False')
-        are parametrized on a linear function, then this boolean decides 
-        whether the intercept can vary across batches.
-    :param random_slope_{parameter}: if parameters mu (default='True'), 
-        sigma (default='False'), epsilon (default='False'), and delta (default='False')
-        are parametrized on a linear function, then this boolean decides 
-        whether the slope can vary across batches.
-    :param centered_intercept_{parameter}: if parameters mu (default='False'), 
-        sigma (default='False'), epsilon (default='False'), and delta (default='False')
-        are parametrized on a linear function, then this boolean decides 
-        whether the parameters of intercept are estimated in a centered or
-        non-centered manner (default). While centered estimation runs faster
-        it may cause some problems for the sampler (the funnel of hell).
-    :param centered_slope_{parameter}: if parameters mu (default='False'), 
-        sigma (default='False'), epsilon (default='False'), and delta (default='False')
-        are parametrized on a linear function, then this boolean decides 
-        whether the parameters of slope are estimated in a centered or
-        non-centered manner (default). While centered estimation runs faster
-        it may cause some problems for the sampler (the funnel of hell).
     :param sampler: specifies the type of PyMC sampler (Defauls is 'NUTS').
     :param n_samples: The number of samples to draw (Default is '1000'). Please 
         note that this parameter must be specified in a string fromat ('1000' and 
@@ -113,49 +78,45 @@ class NormHBR(NormBase):
         in neural network model. It can be either '1' or '2'. The default is set to '2'.
     :param nn_hidden_neuron_num: String that specifies the number of neurons in 
         the hidden layers. The defauls is set to  '2'.
-        
-    Written by S.de Boer and S.M. Kia
     
     """
 
     def __init__(self, **kwargs):
 
         self.configs = dict()
-        # inputs
         self.configs['trbefile'] = kwargs.pop('trbefile', None)
         self.configs['tsbefile'] = kwargs.pop('tsbefile', None)
-        # Model settings
         self.configs['type'] = kwargs.pop('model_type', 'linear')
-        self.configs['random_noise'] = kwargs.pop('random_noise', 'True') == 'True'
         self.configs['likelihood'] = kwargs.pop('likelihood', 'Normal')
-        # sampler settings
+        self.configs['sampler'] = kwargs.pop('sampler', 'NUTS')
         self.configs['n_samples'] = int(kwargs.pop('n_samples', '1000'))
         self.configs['n_tuning'] = int(kwargs.pop('n_tuning', '500'))
         self.configs['n_chains'] = int(kwargs.pop('n_chains', '1'))
-        self.configs['sampler'] = kwargs.pop('sampler', 'NUTS')
-        self.configs['target_accept'] = float(kwargs.pop('target_accept', '0.8'))
-        self.configs['init'] = kwargs.pop('init', 'jitter+adapt_diag')
         self.configs['cores'] = int(kwargs.pop('cores', '1'))
-        # model transfer setting
+        self.configs['init'] = kwargs.pop('init', 'jitter+adapt_diag')
+        self.configs['target_accept'] = float(kwargs.pop('target_accept', '0.8'))
         self.configs['freedom'] = int(kwargs.pop('freedom', '1'))
-        self.configs['transferred'] = False
-        # deprecated settings
-        self.configs['skewed_likelihood'] = kwargs.pop('skewed_likelihood', 'False') == 'True'
-        # misc
-        self.configs['pred_type'] = kwargs.pop('pred_type', 'single')
-
+        self.configs['pred_type'] = kwargs.pop('pred_type', 'single') # ???
+        self.configs['transferred'] = False # Specifies whether this model is transferred or not (is always False when initializing a new model).
+        
+        ### Get deprecated soon
+        #self.configs['skewed_likelihood'] = kwargs.pop('skewed_likelihood', 'False') == 'True'
+        #self.configs['random_noise'] = kwargs.pop('random_noise', 'True') == 'True'
+        #self.configs['hetero_noise'] = kwargs.pop('hetero_noise', 'False') == 'True'
+        ### End of deprecation section
+        
         if self.configs['type'] == 'bspline':
             self.configs['order'] = int(kwargs.pop('order', '3'))
             self.configs['nknots'] = int(kwargs.pop('nknots', '5'))
         elif self.configs['type'] == 'polynomial':
             self.configs['order'] = int(kwargs.pop('order', '3'))
         elif self.configs['type'] == 'nn':
-            self.configs['nn_hidden_neuron_num'] = int(kwargs.pop('nn_hidden_neuron_num', '2'))
             self.configs['nn_hidden_layers_num'] = int(kwargs.pop('nn_hidden_layers_num', '2'))
-            if self.configs['nn_hidden_layers_num'] > 2:
+            self.configs['nn_hidden_neuron_num'] = int(kwargs.pop('nn_hidden_neuron_num', '2'))
+            if self.configs['nn_hidden_layers_num'] > 2 or self.configs['nn_hidden_layers_num'] <= 0:
                 raise ValueError("Using " + str(self.configs['nn_hidden_layers_num']) \
                                  + " layers was not implemented. The number of " \
-                                 + " layers has to be less than 3.")
+                                 + " layers has to be either 1 or 2.")
         elif self.configs['type'] == 'linear':
             pass
         else:
@@ -163,43 +124,64 @@ class NormHBR(NormBase):
                              'polynomial', 'bspline', or 'nn'.")
 
         if self.configs['type'] in ['bspline', 'polynomial', 'linear']:
-
-            for p in ['mu', 'sigma', 'epsilon', 'delta']:
-                self.configs[f'linear_{p}'] = kwargs.pop(f'linear_{p}', 'False') == 'True'
+            
+            self.configs['centered'] = kwargs.pop('centered', 'False') == 'True'
+            self.configs['random_mu'] = kwargs.pop('random_mu', 'True') == 'True'
+            ######## Deprecations (remove in later version)
+            for c in ['mu_linear', 'linear_mu', 'random_intercept', 'random_slope']:
+                if c in kwargs.keys():
+                    print(f'The keyword {c} is deprecated. It is automatically replaced with random_mu.')
+                    self.configs['random_mu'] = kwargs.pop(c, 'True') == 'True'
+            ##### End Deprecations
+            if self.configs['random_mu']:
+                self.configs['random_mu_intercept'] = kwargs.pop('random_mu_intercept', 'True') == 'True'
+                self.configs['random_mu_slope'] = kwargs.pop('random_mu_slope', 'True') == 'True'
+            
+            
+            self.configs['random_sigma'] = kwargs.pop('random_sigma', 'True') == 'True'
+            ######## Deprecations (remove in later version)
+            for c in ['sigma_linear', 'linear_sigma', 'sigma_intercept', 'random_slope']:
+                if c in kwargs.keys():
+                    print(f'The keyword {c} is deprecated. It is automatically replaced with random_mu.')
+                    self.configs['random_sigma'] = kwargs.pop(c, 'True') == 'True'
+            ##### End Deprecations
+            if self.configs['random_sigma']:
+                self.configs['random_sigma_intercept'] = kwargs.pop('random_sigma_intercept', 'False') == 'True'
+                self.configs['random_sigma_slope'] = kwargs.pop('random_sigma_slope', 'False') == 'True'
+            
+            
+            for p in ['epsilon', 'delta']:
+                self.configs[f'random_{p}'] = kwargs.pop(f'random_{p}', 'False') == 'True'
 
                 ######## Deprecations (remove in later version)
-                if f'{p}_linear' in kwargs.keys():
-                    print(f'The keyword \'{p}_linear\' is deprecated. It is now automatically replaced with \'linear_{p}\'')
-                    self.configs[f'linear_{p}'] = kwargs.pop(f'{p}_linear', 'False') == 'True'
+                for c in [f'{p}_linear', f'linear_{p}','random_noise']:
+                    if c in kwargs.keys():
+                        print(f'The keywords {c} is deprecated. It is automatically replaced with \'random_{p}\'')
+                        self.configs[f'random_{p}'] = kwargs.pop(c, 'True') == 'True'
                 ##### End Deprecations 
-
-                for c in ['centered','random']:
-                    self.configs[f'{c}_{p}'] = kwargs.pop(f'{c}_{p}', 'False') == 'True'
-                    for sp in ['slope','intercept']:
-                        self.configs[f'{c}_{sp}_{p}'] = kwargs.pop(f'{c}_{sp}_{p}', 'False') == 'True'
+                
+                if self.configs[f'random_{p}']:
+                    self.configs[f'random_{p}_intercept'] = kwargs.pop(f'random_{p}_intercept', 'True') == 'True'
+                    self.configs[f'random_{p}_slope'] = kwargs.pop(f'random_{p}_slope', 'True') == 'True'
+            
+                #for c in ['centered','random']:
+                #    self.configs[f'{c}_{p}'] = kwargs.pop(f'{c}_{p}', 'False') == 'True'
+                #    for sp in ['slope','intercept']:
+                #        self.configs[f'{c}_{sp}_{p}'] = kwargs.pop(f'{c}_{sp}_{p}', 'False') == 'True'
 
             ######## Deprecations (remove in later version)
-            if self.configs['linear_sigma']:
-                if 'random_noise' in kwargs.keys():
-                    print("The keyword \'random_noise\' is deprecated. It is now automatically replaced with \'random_intercept_sigma\', because sigma is linear")
-                    self.configs['random_intercept_sigma'] = kwargs.pop('random_noise','True') == 'True'
-            elif 'random_noise' in kwargs.keys():
-                print("The keyword \'random_noise\' is deprecated. It is now automatically replaced with \'random_sigma\', because sigma is fixed")
-                self.configs['random_sigma'] = kwargs.pop('random_noise','True') == 'True'
-            if 'random_slope' in kwargs.keys():
-                print("The keyword \'random_slope\' is deprecated. It is now automatically replaced with \'random_intercept_mu\'")
-                self.configs['random_slope_mu'] = kwargs.pop('random_slope','True') == 'True'
+            #if self.configs['linear_sigma']:
+            #    if 'random_noise' in kwargs.keys():
+            #        print("The keyword \'random_noise\' is deprecated. It is now automatically replaced with \'random_intercept_sigma\', because sigma is linear")
+            #        self.configs['random_intercept_sigma'] = kwargs.pop('random_noise','False') == 'True'
+            #elif 'random_noise' in kwargs.keys():
+            #    print("The keyword \'random_noise\' is deprecated. It is now automatically replaced with \'random_sigma\', because sigma is fixed")
+            #    self.configs['random_sigma'] = kwargs.pop('random_noise','False') == 'True'
+            #if 'random_slope' in kwargs.keys():
+            #    print("The keyword \'random_slope\' is deprecated. It is now automatically replaced with \'random_slope_mu\'")
+            #    self.configs['random_slope_mu'] = kwargs.pop('random_slope','False') == 'True'
             ##### End Deprecations 
 
-
-        ## Default parameters
-        self.configs['linear_mu'] = kwargs.pop('linear_mu','True') == 'True'
-        self.configs['random_mu'] = kwargs.pop('random_mu','True') == 'True'
-        self.configs['random_intercept_mu'] = kwargs.pop('random_intercept_mu','True') == 'True'
-        self.configs['random_slope_mu'] = kwargs.pop('random_slope_mu','True') == 'True'
-        self.configs['random_sigma'] = kwargs.pop('random_sigma','True') == 'True'
-        self.configs['centered_sigma'] = kwargs.pop('centered_sigma','True') == 'True'
-        ## End default parameters
 
         self.hbr = HBR(self.configs)
 
