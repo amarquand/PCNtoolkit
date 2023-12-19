95757c0de9748cbd0ee7e3530203869308802998
Stijn de Boer <19709783+AuguB@users.noreply.github.com>
2023-12-19 13:30:21 +0100

    Initial version of empty skeleton

--- pcntoolkit/normative_model/norm_base.py
+++ pcntoolkit/normative_model/norm_base.py
@@ -1,60 +1,226 @@
-import os
-import sys
-from six import with_metaclass
-from abc import ABCMeta, abstractmethod
-import pickle
+from abc import ABC, abstractmethod
+import warnings
 
-try:  # run as a package if installed
-    from pcntoolkit import configs
-except ImportError:
-    pass
-
-    path = os.path.abspath(os.path.dirname(__file__))
-    if path not in sys.path:
-        sys.path.append(path)
-    del path
-    import configs
+from pcntoolkit.dataio.norm_data import NormData
+from pcntoolkit.normative_model.norm_conf import NormConf
 
 
-class NormBase(with_metaclass(ABCMeta)):
-    """ Base class for normative model back-end.
+class NormBase(ABC):  # newer abstract base class syntax, no more python2
 
-        All normative modelling approaches must define the following methods::
+    def __init__(self, name: str, conf: NormConf):
+        self._conf: NormConf = conf
+        self._name: str = name
+        
 
-            NormativeModel.estimate()
-            NormativeModel.predict()
-    """
+    def fit(self, data: NormData):
+        """
+        Contains all the general fitting logic that is not specific to the regression model.
+        This includes cv, logging, saving, etc.
+        """
 
-    def __init__(self, x=None):
+        # some preparations and preprocessing
+        # ...
+
+        self._fit(data)
+
+        # some cleanup and postprocessing
+        # ...
+            
+
+    def predict(self, data: NormData) -> NormData:
+        """
+        Contains all the general prediction logic that is not specific to the regression model.
+        This includes cv, logging, saving, etc.
+        """
+
+        # some preparations and preprocessing
+        # ...
+
+        result = self._predict(data)
+
+        # some cleanup and postprocessing
+        # ...
+            
+        return result
+    
+
+    def fit_predict(self, data: NormData) -> NormData:
+        """
+        Contains all the general fit_predict logic that is not specific to the regression model.
+        This includes cv, logging, saving, etc.
+        """
+
+        # some preparations and preprocessing
+        # ...
+
+        result = self._fit_predict(data)
+
+        # some cleanup and postprocessing
+        # ...
+            
+        return result
+    
+    
+    def transfer(self, data: NormData) -> 'NormBase':
+        """
+        Transfers the normative model to a new dataset.
+        """
+        # some preparations and preprocessing
+        # ...
+        
+        result = self._transfer(data)
+
+        # some cleanup and postprocessing
+        # ...
+            
+        return result
+
+
+    def extend(self, data: NormData):
+        """
+        Extends the normative model with new data.
+        """
+        # some preparations and preprocessing
+        # ...
+
+        result = self._extend(data)
+
+        # some cleanup and postprocessing
+        # ...
+            
+        return result
+    
+    
+    def tune(self, data: NormData):
+        """
+        Tunes the normative model.
+        """
+        # some preparations and preprocessing
+        # ...
+
+        result = self._tune(data)
+        
+        # some cleanup and postprocessing
+        # ...
+            
+        return result
+    
+    
+    def merge(self, other:'NormBase'):
+        """
+        Merges the normative model with another normative model.
+        """
+        # some preparations and preprocessing
+        # ...
+
+        if not self.__class__ == other.__class__:
+            raise ValueError('Attempted to merge two different normative models.')
+
+        result = self._merge(other)
+
+        # some cleanup and postprocessing
+        # ...
+            
+        return result
+
+
+    def evaluate(self, data: NormData):
+        """
+        Contains evaluation logic.
+        """
+        results = {}
+        results['MSE'] = self.evaluate_mse(data)
+        results['MAE'] = self.evaluate_mae(data)
+        results['R2'] = self.evaluate_r2(data)
+        # Add more metrics here, and add the corresponding abstract methods below.
+        return results
+    
+
+    @abstractmethod
+    def _fit_predict(self, data: NormData):
+        """
+        Acts as the adapter for fit_predict using the specific regression model.
+        Is not responsible for cv, logging, saving, etc.
+        """
         pass
 
     @abstractmethod
-    def estimate(self, X, y):
-        """ Estimate the normative model """
+    def _fit(self, data: NormData) -> NormData:
+        """
+        Acts as the adapter for fitting the specific regression model.
+        Is not responsible for cv, logging, saving, etc.
+        """
+        pass
 
     @abstractmethod
-    def predict(self, Xs, X, y):
-        """ Make predictions for new data """
+    def _predict(self, data: NormData) -> NormData:
+        """
+        Acts as the adapter for prediction using the specific regression model.
+        Is not responsible for cv, logging, saving, etc.
+        """
+        pass
 
-    @property
+
     @abstractmethod
-    def n_params(self):
-        """ Report the number of parameters required by the model """
+    def _transfer(self, data: NormData) -> 'NormBase':
+        """
+        Transfers the normative model to a new dataset.
+        """
+        pass
 
-    def save(self, save_path):
-        try:
-            with open(save_path, 'wb') as handle:
-                pickle.dump(self, handle, protocol=configs.PICKLE_PROTOCOL)
-            return True
-        except Exception as err:
-            print('Error:', err)
-            raise
 
-    def load(self, load_path):
-        try:
-            with open(load_path, 'rb') as handle:
-                nm = pickle.load(handle)
-            return nm
-        except Exception as err:
-            print('Error:', err)
-            raise
+    @abstractmethod
+    def _extend(self, data: NormData):
+        """
+        Extends the normative model with new data.
+        """
+        pass
+
+    @abstractmethod
+    def _tune(self, data: NormData):
+        """
+        Tunes the normative model.
+        """
+        pass
+
+    @abstractmethod
+    def _merge(self, other:'NormBase'):
+        """
+        Merges the normative model with another normative model.
+        """
+        pass
+
+    @abstractmethod
+    def evaluate_mse(self, data: NormData) -> float:
+        """
+        Evaluates the model using MSE.
+        """
+        pass
+
+    @abstractmethod
+    def evaluate_mae(self, data: NormData) -> float:
+        """
+        Evaluates the model using MAE.
+        """
+        pass
+
+    @abstractmethod
+    def evaluate_r2(self, data: NormData) -> float:
+        """
+        Evaluates the model using R2.
+        """
+        pass
+
+    @abstractmethod
+    def save(self):
+        """
+        Saves the model to the specified directory.
+        """
+        pass
+    
+    @abstractmethod
+    def load(self) -> 'NormBase':
+        """
+        Loads the model from the specified directory.
+        """
+        pass
