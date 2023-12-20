bb353c73f0792e6a0d4895f472d8984cd3c54ddb
Stijn de Boer <19709783+AuguB@users.noreply.github.com>
2023-12-20 10:13:56 +0100

    Initial commit.

--- normative.py
+++ normative.py
@@ -3,6 +3,7 @@ import os
 from pcntoolkit.normative_model.norm_conf import NormConf
 from pcntoolkit.normative_model.norm_factory import create_normative_model
 from pcntoolkit.regression_model.blr.blr_conf import BLRConf
+from pcntoolkit.regression_model.hbr.hbr_conf import HBRConf
 
 
 
@@ -11,7 +12,7 @@ def main():
     save_dir = "/home/stijn/temp/pcntoolkit/save"
     normconf = NormConf(perform_cv = False, cv_folds=0, log_dir=log_dir, save_dir=save_dir)
     normconf.save_as_json("/home/stijn/temp/pcntoolkit/normconf.json")
-    regconf = BLRConf(example_parameter=-1)
+    regconf = HBRConf(example_parameter=-1)
 
     normative_model = create_normative_model(normconf, regconf)
     normative_model.fit_predict(None)
