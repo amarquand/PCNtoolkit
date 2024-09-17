292a84b9c7de6a420f27112058fb9a5f652a3057
AuguB <stijn6@proton.me>
2024-09-17 16:20:19 +0200

    Starting work on blr

--- pcntoolkit/regression_model/blr/blr_conf.py
+++ pcntoolkit/regression_model/blr/blr_conf.py
@@ -1,13 +1,16 @@
 from dataclasses import dataclass
 
+from pcntoolkit.regression_model.blr.warp import WarpBase, WarpAffine, WarpCompose, WarpLog, WarpSinArcsinh, WarpBoxCox
 from pcntoolkit.regression_model.reg_conf import RegConf
-
 
 @dataclass(frozen=True)
 class BLRConf(RegConf):
     # some configuration parameters
     n_iter: int = 100
     tol: float = 1e-3
+    var_groups: list = None
+    warp: WarpBase = None
+    warp_reparam: bool = False
 
     def detect_configuration_problems(self) -> str:
         """
@@ -15,7 +18,7 @@ class BLRConf(RegConf):
         The super class will throw an exception if the configuration is invalid, and show the problems.
         """
 
-        # DESIGN CHOICE (stijn):
+        # DESIGN CHOICE (stijn)
         # This mutable field need to be local here, because the dataclass is defined as immutable.
         configuration_problems = []
 
@@ -23,10 +26,13 @@ class BLRConf(RegConf):
             nonlocal configuration_problems
             configuration_problems.append(f"{problem}")
 
-        # some configuration checks
-        # ...
-        if self.example_parameter < 0:
-            add_problem("Example parameter must be greater than 0.")
+        if self.n_iter < 1:
+            add_problem("n_iter must be greater than 0.")
+
+        if self.tol <= 0:
+            add_problem("tol must be greater than 0.")
+
+        
 
         return configuration_problems
 
