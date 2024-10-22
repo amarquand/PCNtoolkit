a308e782ca37e6b964798be85430bfbb7867d134
Augub <augustijndeboer@gmail.com>
2024-10-22 14:30:02 +0200

    Set default argument for multicore on HPC

--- pcntoolkit/normative_parallel.py
+++ pcntoolkit/normative_parallel.py
@@ -112,7 +112,7 @@ def execute_nm(processing_dir,
     cluster_spec = kwargs.pop('cluster_spec', 'torque')
     log_path = kwargs.get('log_path', None)
     binary = kwargs.pop('binary', False)
-    cores = kwargs.pop('n_cores_per_batch')
+    cores = kwargs.pop('n_cores_per_batch','1')
 
     split_nm(processing_dir,
              respfile_path,
