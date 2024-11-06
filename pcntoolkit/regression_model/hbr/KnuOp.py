0e89c2cbc00b9a5644c0d327e9ebe3a0447697c4
Stijn <stijn6@proton.me>
2024-11-06 10:01:02 +0100

    More docstring

--- pcntoolkit/regression_model/hbr/KnuOp.py
+++ pcntoolkit/regression_model/hbr/KnuOp.py
@@ -1,48 +1,200 @@
 # Third-party imports
-import scipy.special as spp
+from typing import Any, List, Sequence, Union
+
+import scipy.special as spp  # type: ignore
 from pytensor.gradient import grad_not_implemented
+from pytensor.graph.basic import Apply
 from pytensor.scalar.basic import BinaryScalarOp, upgrade_to_float
+from pytensor.tensor.var import Variable
 
 
 class KnuOp(BinaryScalarOp):
-    """
-    Modified Bessel function of the second kind, pytensor wrapper for scipy.special.kv
+    """Modified Bessel function of the second kind, PyTensor wrapper for scipy.special.kv.
+
+    This class implements a PyTensor operation for computing the modified Bessel function
+    of the second kind (K_nu(x)). It wraps scipy.special.kv to provide automatic
+    differentiation capabilities within PyTensor computational graphs.
+
+    Parameters
+    ----------
+    dtype_converter : callable
+        Function to convert input types (typically upgrade_to_float)
+    name : str
+        Name of the operation
+
+    Notes
+    -----
+    The modified Bessel function K_nu(x) is a solution to the modified Bessel
+    differential equation. This implementation supports automatic differentiation
+    with respect to both the order (nu) and the argument (x).
+
+    See Also
+    --------
+    scipy.special.kv : The underlying modified Bessel function implementation
+    KnuPrimeOp : Derivative of the modified Bessel function
     """
 
     nfunc_spec = ("scipy.special.kv", 2, 1)
 
     @staticmethod
-    def st_impl(p, x):
+    def st_impl(p: Union[float, int], x: Union[float, int]) -> float:
+        """Static implementation of the modified Bessel function.
+
+        Parameters
+        ----------
+        p : float or int
+            Order of the modified Bessel function
+        x : float or int
+            Argument where the function is evaluated
+
+        Returns
+        -------
+        float
+            Value of the modified Bessel function K_p(x)
+        """
         return spp.kv(p, x)
 
-    def impl(self, p, x):
+    def impl(self, p: Union[float, int], x: Union[float, int]) -> float:
+        """Implementation of the modified Bessel function.
+
+        Parameters
+        ----------
+        p : float or int
+            Order of the modified Bessel function
+        x : float or int
+            Argument where the function is evaluated
+
+        Returns
+        -------
+        float
+            Value of the modified Bessel function K_p(x)
+        """
         return KnuOp.st_impl(p, x)
 
-    def grad(self, inputs, grads):
+    def grad(
+        self,
+        inputs: Sequence[Variable[Any, Any]],
+        output_gradients: Sequence[Variable[Any, Any]],
+    ) -> List[Variable]:
+        """Compute gradients of the modified Bessel function.
+
+        Parameters
+        ----------
+        inputs : list of Variables
+            List containing the order (p) and argument (x)
+        output_grads : list of Variables
+            List containing the gradient with respect to the output
+
+        Returns
+        -------
+        list of Variables
+            Gradients with respect to p and x
+
+        Notes
+        -----
+        The gradient with respect to p is computed using finite differences
+        due to the lack of a closed-form expression.
+        """
         dp = 1e-16
         (p, x) = inputs
-        (gz,) = grads
-        dfdp = (knuop(p + dp, x) - knuop(p - dp, x)) / (2 * dp)
-        return [gz * dfdp, gz * knupop(p, x)]
+        (gz,) = output_gradients
+        # Use finite differences for derivative with respect to p
+        dfdp = (knuop(p + dp, x) - knuop(p - dp, x)) / (2 * dp)  # type: ignore
+        return [gz * dfdp, gz * knupop(p, x)]  # type: ignore
+
+    def c_code(
+        self,
+        node: Apply[Any],
+        name: str,
+        inputs: Sequence[Any],
+        outputs: Sequence[Any],
+        sub: dict[str, str],
+    ) -> Any:
+        raise NotImplementedError("C code generation not implemented for KnuOp")
 
 
 class KnuPrimeOp(BinaryScalarOp):
-    """
-    Derivative of the modified Bessel function of the second kind.
+    """Derivative of the modified Bessel function of the second kind.
+
+    This class implements a PyTensor operation for computing the derivative of the
+    modified Bessel function of the second kind with respect to its argument.
+    It wraps scipy.special.kvp.
+
+    Parameters
+    ----------
+    dtype_converter : callable
+        Function to convert input types (typically upgrade_to_float)
+    name : str
+        Name of the operation
     """
 
     nfunc_spec = ("scipy.special.kvp", 2, 1)
 
     @staticmethod
-    def st_impl(p, x):
+    def st_impl(p: Union[float, int], x: Union[float, int]) -> float:
+        """Static implementation of the Bessel function derivative.
+
+        Parameters
+        ----------
+        p : float or int
+            Order of the modified Bessel function
+        x : float or int
+            Argument where the derivative is evaluated
+
+        Returns
+        -------
+        float
+            Value of the derivative K'_p(x)
+        """
         return spp.kvp(p, x)
 
-    def impl(self, p, x):
+    def impl(self, p: Union[float, int], x: Union[float, int]) -> float:
+        """Implementation of the Bessel function derivative.
+
+        Parameters
+        ----------
+        p : float or int
+            Order of the modified Bessel function
+        x : float or int
+            Argument where the derivative is evaluated
+
+        Returns
+        -------
+        float
+            Value of the derivative K'_p(x)
+        """
         return KnuPrimeOp.st_impl(p, x)
 
-    def grad(self, inputs, grads):
+    def grad(
+        self, inputs: Sequence[Variable[Any, Any]], grads: Sequence[Variable[Any, Any]]
+    ) -> List[Variable]:
+        """Compute gradients of the Bessel function derivative.
+
+        Parameters
+        ----------
+        inputs : list of Variables
+            List containing the order (p) and argument (x)
+        grads : list of Variables
+            List containing the gradient with respect to the output
+
+        Returns
+        -------
+        list of Variables
+            Gradients with respect to p and x (not implemented)
+        """
         return [grad_not_implemented(self, 0, "p"), grad_not_implemented(self, 1, "x")]
 
+    def c_code(
+        self,
+        node: Apply[Any],
+        name: str,
+        inputs: Sequence[Any],
+        outputs: Sequence[Any],
+        sub: dict[str, str],
+    ) -> Any:
+        raise NotImplementedError("C code generation not implemented for KnupOp")
 
+
+# Create operation instances
 knuop = KnuOp(upgrade_to_float, name="knuop")
 knupop = KnuPrimeOp(upgrade_to_float, name="knupop")
--- pcntoolkit/regression_model/hbr/hbr_data.py
+++ pcntoolkit/regression_model/hbr/hbr_data.py
@@ -1,22 +1,92 @@
+"""
+Data container and processor for Hierarchical Bayesian Regression (HBR) models.
+
+This module provides the HBRData class which handles data preparation, validation,
+and integration with PyMC models for hierarchical Bayesian regression. It manages
+covariates, response variables, and batch effects while providing utilities for
+data transformation and model integration.
+
+Notes
+-----
+The module requires PyMC for Bayesian modeling integration and NumPy for array operations.
+All data is validated and transformed into appropriate formats for use in HBR models.
+"""
+
 import warnings
 from collections import OrderedDict
-from typing import Any, List
+from typing import Any, Dict, List, Optional, Tuple
 
 import numpy as np
-import pymc as pm
+import pymc as pm  # type: ignore
 
 
 class HBRData:
+    """
+    A data container class for Hierarchical Bayesian Regression models.
+
+    This class handles the preparation and management of data for HBR models,
+    including covariates, response variables, and batch effects. It provides
+    functionality for data validation, transformation, and integration with
+    PyMC models.
+
+    Parameters
+    ----------
+    X : np.ndarray
+        Covariate matrix of shape (n_samples, n_features)
+    y : np.ndarray, optional
+        Response variable array of shape (n_samples,) or (n_samples, 1)
+    batch_effects : np.ndarray, optional
+        Batch effects matrix of shape (n_samples, n_batch_effects)
+    response_var : str, optional
+        Name of the response variable
+    covariate_dims : List[str], optional
+        Names of the covariate dimensions
+    batch_effect_dims : List[str], optional
+        Names of the batch effect dimensions
+    datapoint_coords : List[Any], optional
+        Coordinates for each datapoint
+
+    Attributes
+    ----------
+    X : np.ndarray
+        Processed covariate matrix
+    y : np.ndarray
+        Processed response variable array
+    batch_effects : np.ndarray
+        Processed batch effects matrix
+    response_var : str
+        Name of the response variable
+    covariate_dims : List[str]
+        Names of covariate dimensions
+    batch_effect_dims : List[str]
+        Names of batch effect dimensions
+    pm_X : pm.Data
+        PyMC Data container for covariates
+    pm_y : pm.Data
+        PyMC Data container for response variable
+    pm_batch_effect_indices : Tuple[pm.Data, ...]
+        PyMC Data containers for batch effect indices
+
+    Examples
+    --------
+    >>> X = np.random.randn(100, 5)
+    >>> y = np.random.randn(100)
+    >>> batch_effects = np.random.randint(0, 3, (100, 2))
+    >>> data = HBRData(X, y, batch_effects)
+    >>> with pm.Model() as model:
+    ...     data.add_to_graph(model)
+    """
+
     def __init__(
         self,
         X: np.ndarray,
-        y: np.ndarray = None,
-        batch_effects: np.ndarray = None,
-        response_var: str = None,
-        covariate_dims: List = None,
-        batch_effect_dims: List = None,
-        datapoint_coords: List = None,
-    ):
+        y: Optional[np.ndarray] = None,
+        batch_effects: Optional[np.ndarray] = None,
+        response_var: Optional[str] = None,
+        covariate_dims: Optional[List[str]] = None,
+        batch_effect_dims: Optional[List[str]] = None,
+        datapoint_coords: Optional[List[Any]] = None,
+    ) -> None:
         self.check_and_set_data(X, y, batch_effects)
 
         # Set the response var
@@ -31,28 +101,23 @@ class HBRData:
         self._coords = OrderedDict()  # This preserves the order of the keys
 
         # Create datapoint coordinates
-        if datapoint_coords is None:
-            self._coords["datapoints"] = list(np.arange(self._n_datapoints))
-        else:
-            self._coords["datapoints"] = datapoint_coords
+        self._coords["datapoints"] = datapoint_coords or list(
+            np.arange(self._n_datapoints)
+        )
 
         # Create covariate dims if they are not provided
-        self.covariate_dims = covariate_dims
-        if self.covariate_dims is None:
-            self.covariate_dims = [
-                "covariate_" + str(i) for i in range(self._n_covariates)
-            ]
+        self.covariate_dims = covariate_dims or [
+            "covariate_" + str(i) for i in range(self._n_covariates)
+        ]
         assert (
             len(self.covariate_dims) == self._n_covariates
         ), "The number of covariate dimensions must match the number of covariates"
         self._coords["covariates"] = self.covariate_dims
 
         # Create batch_effect dims if they are not provided
-        self.batch_effect_dims = batch_effect_dims
-        if self.batch_effect_dims is None:
-            self.batch_effect_dims = [
-                "batch_effect_" + str(i) for i in range(self._n_batch_effect_columns)
-            ]
+        self.batch_effect_dims = batch_effect_dims or [
+            "batch_effect_" + str(i) for i in range(self._n_batch_effect_columns)
+        ]
         assert (
             len(self.batch_effect_dims) == self._n_batch_effect_columns
         ), "The number of batch effect dimensions must match the number of batch effect columns"
@@ -65,11 +130,46 @@ class HBRData:
             self._batch_effects_maps[v] = {w: j for j, w in enumerate(be_values)}
             self._coords[v] = be_values
 
+        self.pm_X: pm.Data = None  # type: ignore
+        self.pm_y: pm.Data = None  # type:ignore
+        self.pm_batch_effect_indices: Tuple[pm.Data, ...] = None  # type: ignore
+
         self.create_batch_effect_indices()
 
-    def check_and_set_data(self, X, y, batch_effects):
+    def check_and_set_data(
+        self,
+        X: np.ndarray,
+        y: Optional[np.ndarray],
+        batch_effects: Optional[np.ndarray],
+    ) -> None:
         """
-        Checks that the data is valid and sets the data attributes.
+        Validate and set the input data arrays.
+
+        Performs validation checks on the input arrays and sets them as instance
+        attributes. Ensures proper dimensionality and compatibility between arrays.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            Covariate matrix of shape (n_samples, n_features)
+        y : np.ndarray or None
+            Response variable array of shape (n_samples,) or (n_samples, 1).
+            If None, creates zero array of appropriate shape.
+        batch_effects : np.ndarray or None
+            Batch effects matrix of shape (n_samples, n_batch_effects).
+            If None, creates zero array of appropriate shape.
+
+        Raises
+        ------
+        ValueError
+            If X is None
+        AssertionError
+            If array dimensions are incompatible or y has incorrect shape
+
+        Notes
+        -----
+        - Automatically expands 1D arrays to 2D
+        - Squeezes y to 1D if it's a 2D array with one column
         """
 
         if X is None:
@@ -104,7 +204,17 @@ class HBRData:
 
     def add_to_graph(self, model: pm.Model) -> None:
         """
-        Add the data to the pymc model graph using the model context.
+        Add data variables to a PyMC model graph.
+
+        Parameters
+        ----------
+        model : pm.Model
+            PyMC model to add the data variables to
+
+        Notes
+        -----
+        Creates PyMC Data objects for X, y, and batch effect indices within
+        the model context. Also adds custom batch effect dimensions to the model.
         """
         with model:
             self.pm_X = pm.Data("X", self.X, dims=("datapoints", "covariates"))
@@ -120,11 +230,21 @@ class HBRData:
                 ]
             )
 
-            model.custom_batch_effect_dims = self.batch_effect_dims
+            model.custom_batch_effect_dims = self.batch_effect_dims  # type: ignore
 
     def set_data_in_existing_model(self, model: pm.Model) -> None:
         """
-        Sets the data in an existing pymc model.
+        Update data values in an existing PyMC model.
+
+        Parameters
+        ----------
+        model : pm.Model
+            Existing PyMC model whose data values need to be updated
+
+        Notes
+        -----
+        Updates the values of X, y, and batch effect indices in the model
+        while preserving the model structure.
         """
         model.set_data(
             "X",
@@ -142,16 +262,44 @@ class HBRData:
             be_acc.append(model[self.batch_effect_dims[i] + "_data"])
         self.pm_batch_effect_indices = tuple(be_acc)
 
-    def expand_all(self, *args):
+    def expand_all(self, *args: str) -> Tuple[np.ndarray, ...]:
         """
-        Expands all data attributes.
+        Expand multiple data attributes to 2D arrays if necessary.
+
+        Parameters
+        ----------
+        *args : str
+            Names of attributes to expand
+
+        Returns
+        -------
+        Tuple[np.ndarray, ...]
+            Tuple of expanded arrays
+
+        See Also
+        --------
+        expand : Method for expanding individual arrays
         """
         return tuple(self.expand(arg) for arg in args)
 
     def expand(self, data_attr_str: str) -> np.ndarray:
         """
-        Expands a 1D array to a 2D array if necessary.
-        Raises an error if the array is not 1D or 2D.
+        Expand a single data attribute to a 2D array if necessary.
+
+        Parameters
+        ----------
+        data_attr_str : str
+            Name of the attribute to expand
+
+        Returns
+        -------
+        np.ndarray
+            Expanded array
+
+        Raises
+        ------
+        AssertionError
+            If the array is not 1D or 2D
         """
         data_attr: np.ndarray = getattr(self, data_attr_str)
         if len(data_attr.shape) == 1:
@@ -159,16 +307,35 @@ class HBRData:
         assert len(data_attr.shape) == 2, f"{data_attr_str} must be a 1D or 2D array"
         return data_attr
 
-    def set_batch_effects_maps(self, batch_effects_maps: dict[str, dict[Any, int]]):
+    def set_batch_effects_maps(
+        self, batch_effects_maps: Dict[str, Dict[Any, int]]
+    ) -> None:
         """
-        Sets the batch effects map.
+        Set the mapping between batch effect values and their indices.
+
+        Parameters
+        ----------
+        batch_effects_maps : Dict[str, Dict[Any, int]]
+            Mapping from batch effect names to value-index dictionaries
+
+        Notes
+        -----
+        Updates the batch effect indices after setting the new maps.
         """
         self._batch_effects_maps = batch_effects_maps
         self.create_batch_effect_indices()
 
-    def create_batch_effect_indices(self):
+    def create_batch_effect_indices(self) -> None:
         """
-        Create a list of indices of the batch effects. Allows for indexing the batch effects in the pymc model.
+        Create numerical indices for batch effects.
+
+        Creates a list of arrays where each array contains the numerical indices
+        corresponding to the categorical batch effect values. These indices are
+        used for indexing in the PyMC model.
+
+        Notes
+        -----
+        The indices are created based on the current batch_effects_maps.
         """
         self.batch_effect_indices = []
         for i, v in enumerate(self.batch_effect_dims):
@@ -178,22 +345,24 @@ class HBRData:
                 )
             )
 
+    # pylint: disable=C0116
+
     @property
-    def n_covariates(self):
+    def n_covariates(self) -> int:
         return self._n_covariates
 
     @property
-    def n_datapoints(self):
+    def n_datapoints(self) -> int:
         return self._n_datapoints
 
     @property
-    def n_batch_effect_columns(self):
+    def n_batch_effect_columns(self) -> int:
         return self._n_batch_effect_columns
 
     @property
-    def coords(self):
+    def coords(self) -> OrderedDict:
         return self._coords
 
     @property
-    def batch_effects_maps(self):
+    def batch_effects_maps(self) -> Dict[str, Dict[Any, int]]:
         return self._batch_effects_maps
