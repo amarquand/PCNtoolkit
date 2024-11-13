f8c4865aaf895440f322261db206606a84ac37f6
Stijn <stijn6@proton.me>
2024-11-13 18:02:21 +0100

    Add nicer entrypoint for normative and basic cli testing

---
+++ tests/cli_test/test_cli.sh
@@ -0,0 +1,18 @@
+# Assign the current directory to a variable
+export testdir=$(pwd)
+export tempdir="$testdir/temp"
+mkdir $tempdir
+echo $tempdir
+export data_name="fcon1000"
+export model_config="-a blr warp=WarpSinArcsinh optimizer=l-bfgs-b warp_reparam=True"
+echo "Downloading the data..."
+curl -o $tempdir/$data_name https://raw.githubusercontent.com/predictive-clinical-neuroscience/PCNtoolkit-demo/refs/heads/main/data/$data_name.csv
+echo "Splitting the data into train and test covariates, responses and batch effects..."
+python split_data.py --input_file $tempdir/$data_name --output_dir $tempdir
+echo "Fitting the model..."
+normative $tempdir/Y_tr_$data_name.pkl -c $tempdir/X_tr_$data_name.pkl -f fit $model_config
+echo "Predicting the test set..."
+normative $tempdir/Y_te_$data_name.pkl -c $tempdir/X_te_$data_name.pkl -f predict $model_config inputsuffix=fit outputsuffix=predict
+echo "Also doing estimate..."
+normative $tempdir/Y_tr_$data_name.pkl -c $tempdir/X_tr_$data_name.pkl -f estimate $model_config -t $tempdir/X_te_$data_name.pkl -r $tempdir/Y_te_$data_name.pkl outputsuffix=estimate
+echo "Done!"
